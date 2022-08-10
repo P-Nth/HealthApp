c94ee7bd8f768b7d401378f9ff3b87945bf32c8d
P-Nth <philemonondigo@protonmail.com>
2022-08-10 20:37:23 +0300

    initial commit

---
+++ healthapp/src/Pages/Home/landpage/Output.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Head from "./Pages/Home/main";
+import Intro from "./Pages/Home/main";
+
+function Output() {
+  return (
+    <div className="App">
+      <Head />
+      <Intro />
+    </div>
+  );
+}
+
+export default Output;
---
+++ healthapp/src/Pages/Home/main.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Landpage from "./landpage/Output";
+
+function Main() {
+  return (
+    <div className="App">
+      <Landpage />
+    </div>
+  );
+}
+
+export default Main;
